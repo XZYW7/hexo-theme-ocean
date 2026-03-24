50d8058c7416d3f77a91d27c94712637314164d5
XZYW <928144597@qq.com>
2026-03-25 00:44:53 +0800

    Restore path tracer with lighting

--- source/js/cornell-box.js
+++ source/js/cornell-box.js
@@ -312,16 +312,39 @@
         return vec3(0);
       }
 
-      if (hit.t > 1000.0) {
-        return vec3(1, 0, 0);
+      if (hit.material.emission.r > 0.0 || hit.material.emission.g > 0.0 || hit.material.emission.b > 0.0) {
+        return hit.material.emission;
       }
-      if (hit.t > 500.0) {
-        return vec3(0, 1, 0);
+
+      vec3 normal = hit.normal;
+      if (dot(ray.direction, normal) > 0.0) {
+        normal = -normal;
       }
-      if (hit.t > 100.0) {
-        return vec3(0, 0, 1);
+
+      vec3 lightPos = vec3(0, 4.99, 0);
+      vec3 toLight = lightPos - hit.point;
+      float dist2 = dot(toLight, toLight);
+      vec3 lightDir = toLight / sqrt(dist2);
+      float lightDist = sqrt(dist2);
+
+      Ray shadowRay;
+      shadowRay.origin = hit.point + normal * EPSILON * 2.0;
+      shadowRay.direction = lightDir;
+      Hit shadowHit = intersectScene(shadowRay);
+
+      vec3 surfaceColor = vec3(0);
+
+      if (shadowHit.hit && shadowHit.t < lightDist) {
+        surfaceColor = hit.material.albedo * 0.05;
+      } else {
+        float NdotL = max(dot(normal, lightDir), 0.0);
+        float lightArea = 16.0;
+        float lightIntensity = 15.0;
+        vec3 radiance = hit.material.albedo * NdotL * lightIntensity * lightArea / (PI * dist2);
+        surfaceColor = radiance;
       }
-      return vec3(1, 1, 1);
+
+      return surfaceColor;
     }
 
     mat3 setCamera(vec3 ro, vec3 ta) {
@@ -338,7 +361,12 @@
       mat3 cam = setCamera(u_cameraPos, u_cameraTarget);
       vec3 rd = cam * normalize(vec3(uv, 1.5));
 
-      gl_FragColor = vec4(rd * 0.5 + 0.5, 1.0);
+      Ray ray;
+      ray.origin = u_cameraPos;
+      ray.direction = rd;
+
+      vec3 col = trace(ray);
+      gl_FragColor = vec4(col, 1.0);
     }
   `;
 
